473956942f74560be04af13b97bb7d8382a5abc2
Ali-Khateb-1 <ali.khateb@gmail.com>
2025-06-21 03:48:46 +0300

    💡 Add demo files, sync script, and update styles

---
+++ sync_shared.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "⏬ جارٍ تحميل utils.js من GitHub..."
+
+curl -s -o utils.js https://raw.githubusercontent.com/Ali-Khateb-1/shared-resources/main/utils.js
+
+echo "✅ تم التحديث! تم جلب آخر نسخة من utils.js إلى my-online-store."
+
+echo "⏬ جارٍ تحميل common.css من GitHub..."
+curl -s -o common.css https://raw.githubusercontent.com/Ali-Khateb-1/shared-resources/main/common.css
+echo "✅ تم التحديث! تم جلب common.css."
