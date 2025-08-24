6b6ae7ab71de78222ebee7d05c001f6a6273642f
Ali Khateb | AI-Driven STEAM Innovator for Sustainable Systems 🌿 <eng.ali.khateb@gmail.com>
2025-08-24 23:16:07 +0300

    Create config.js

---
+++ Pizza_Hot/config.js
@@ -0,0 +1,8 @@
+const config = {
+  whatsappNumber: "00972569788731",
+  facebookPage: "https://www.facebook.com/61574045938687",
+  storeName: "PIZZA HOT",
+  currency: "₪",
+  deliveryFee: 10,
+  promoCoupon: "HOT2025"
+};
